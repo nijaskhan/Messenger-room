a7c3146d1c0cc64cdbdddaed1a8202bad2050c32
nijaskhan <nijaskhan93@gmail.com>
2023-06-22 10:58:16 +0530

    updated server for deployment test

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://messenger-room-xi.vercel.app',
         methods: ['GET',  'POST']
     }
 });
