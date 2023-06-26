b817cab18660f17302b6895c1bc1bf80bad4d593
nijaskhan <nijaskhan93@gmail.com>
2023-06-26 10:10:46 +0530

    To prod mode

--- server/server.js
+++ server/server.js
@@ -12,8 +12,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
-        // origin: 'https://messenger-room-xi.vercel.app',
+        // origin: 'http://localhost:3000',
+        origin: 'https://messenger-room-xi.vercel.app',
         methods: ['GET', 'POST']
     }
 });
