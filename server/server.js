df8796bdb77ee040ac73e46551e4f0f54eeefccf
nijaskhan <nijaskhan93@gmail.com>
2023-07-11 09:50:05 +0530

    changed frontent server

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         // origin: 'http://localhost:3000',
-        origin: 'https://messenger-room-git-master-nijaskhan.vercel.app/',
+        origin: ['https://messenger-room-git-master-nijaskhan.vercel.app', 'http://messenger-room-xi.vercel.app'],
         methods: ['GET', 'POST']
     }
 });
