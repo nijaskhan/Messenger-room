af7f6bc60411d4bc9c8ea1ca38c43ebca2908566
nijaskhan <nijaskhan93@gmail.com>
2023-08-16 11:59:54 +0530

    to prod

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'http://localhost:5000';
-// export const BASE_URL = 'https://messenger-server-6jy4.onrender.com';
+// export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://messenger-server-6jy4.onrender.com';
